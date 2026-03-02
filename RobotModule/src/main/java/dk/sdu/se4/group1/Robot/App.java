f626dac9ab41220967edc42dbf0424e6e9a9e73f
FrederikThy <frederik151100@gmail.com>
2026-03-02 13:19:16 +0100

    Initial project creation and one creation of a module - RobotModule

---
+++ RobotModule/src/main/java/dk/sdu/se4/group1/Robot/App.java
@@ -0,0 +1,10 @@
+package dk.sdu.se4.group1.Robot;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
