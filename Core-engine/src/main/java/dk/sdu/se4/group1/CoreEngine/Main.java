8aa8ca4a593ca673ffe6b6952de071c8da23cba0
Jona615m <joole24@student.sdu.dk>
2026-03-04 10:44:35 +0100

    nemt nemt nemt nemt

--- Core-engine/src/main/java/dk/sdu/se4/group1/CoreEngine/Main.java
+++ Core-engine/src/main/java/dk/sdu/se4/group1/CoreEngine/Main.java
@@ -1,10 +1,28 @@
 package dk.sdu.se4.group1.CoreEngine;
 
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 /**
  * Hello world!
  */
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        StackPane root = new StackPane();
+
+        var scene = new Scene(root, 800, 600);
+
+        window.setTitle("RAWR");
+        window.setScene(scene);
+        window.show();
     }
 }
---
+++ Core-engine/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module Core.engine {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+    requires spring.aop;
+    requires spring.expression;
+
+    exports dk.sdu.se4.group1.CoreEngine;
+}
+
