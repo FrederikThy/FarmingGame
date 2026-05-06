d2508e157b0f099a8ea928313439041a4d062815
omarh <ombab24@student.sdu.dk>
2026-05-06 21:00:09 +0200

    GrowthComponent tests

---
+++ CropsModule/src/test/java/dk/sdu/se4/group1/Crops/CropsComponentTests.java
@@ -0,0 +1,30 @@
+package dk.sdu.se4.group1.Crops;
+
+import dk.sdu.se4.group1.CommonApi.SeedType;
+import dk.sdu.se4.group1.CommonEcs.Components.CropComponent;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GrowthComponentTest {
+
+    /** Test for initial values of each crop */
+    @Test
+    void growthStageTest() {
+        GrowthComponent growth = new GrowthComponent();
+
+        assertEquals(1,   growth.growthStage,"Initial growthStage must be 1");
+        assertEquals(0.0, growth.elapsedGrowthTime, "Initial elapsedGrowthTime must be 0");
+        assertEquals(5.0, growth.growthTime,"Default growthTime must be 5 seconds");
+    }
+
+    /** Test for stage changing time */
+    @Test
+    void elapsedTimeTest() {
+        GrowthComponent growth = new GrowthComponent();
+        growth.elapsedGrowthTime += 4.9;
+
+        assertEquals(1, growth.growthStage,
+                "Stage must remain 1 when elapsed time has not reached growthTime");
+    }
+}
